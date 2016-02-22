11fbcae265599c674c1b7e75b96524563bfbda25
Ryan <snappas@gmail.com>
2016-02-22 17:18:25 -0600

    fixed crossover offspring

--- Assignment1/source/main.cpp
+++ Assignment1/source/main.cpp
@@ -14,9 +14,9 @@ void print_population(deque<vector<bool>> population);
 
 int main(int argc, char **argv){
 
-    size_t chromosomePopulationSize = 20; //Size of the population
+    size_t chromosomePopulationSize = 20; //Size o49f the population
     size_t chromosomeLength = 10; //Length of each chromosome
-    double percentCrossOver = 0.0; //Percent of population used for crossover
+    double percentCrossOver = 0.7; //Percent of population used for crossover
     vector<bool> bitmask{1, 1, 1, 1, 1, 0, 0, 0, 0, 0}; //Bitmask used for crossover
 
     //Integer total of chromosomes to use for crossover each generation
@@ -33,15 +33,20 @@ int main(int argc, char **argv){
     int g = 1;
     //Perform crossover and mutation until the goal is reached
     while (!population.checkForGoal()) {
+        vector<Chromosome> generation_offspring;
         //Perform the crossover 'crossOverTotal / 2' times since two chromosomes are used each time
         for (int i = 0; i < crossOverTotal / 2; i++) {
             //Produce two offspring from two parents in single_crossover
             vector<Chromosome> offspring = GeneticAlgorithm::single_crossover(population.selectFrontParent(),
                                                                               population.selectBackParent(),
                                                                               bitmask);
-            //Insert the two offspring into the population
-            population.insertOffspring(offspring);
+            //Add the two new offspring to the generation's offspring
+            for (Chromosome c : offspring) {
+                generation_offspring.push_back(c);
+            }
         }
+        //Insert the generation's offspring into the population
+        population.insertOffspring(generation_offspring);
         g++;
         cout << "Generation " << g << " After Crossover:" << endl;
         print_population(population.get());
